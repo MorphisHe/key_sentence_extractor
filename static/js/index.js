f94497d466677ebd2d3172086fff90ec561e0ed0
JiangHe <jiangh0124@gmail.com>
2020-11-13 13:26:41 -0500

    added horizontal dragger

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,11 @@
-
-// dragger handler
-var dragging = false;
-$("#dragbar").mousedown(function (e) {
+// col-dragger handler
+var col_dragging = false;
+$("#col-dragbar").mousedown(function (e) {
   e.preventDefault();
-  dragging = true;
+  col_dragging = true;
 
   $(document).mousemove(function (e) {
-    if (dragging) {
+    if (col_dragging) {
       var percentage = (e.pageX / window.innerWidth) * 100;
       var contLeftPercentage = 100 - percentage;
 
@@ -17,8 +16,30 @@ $("#dragbar").mousedown(function (e) {
   });
 });
 
-// show pdf-container when not dragging
+// row-dragger handler
+var row_dragging = false;
+$("#row-dragbar").mousedown(function (e) {
+    e.preventDefault();
+    row_dragging = true;
+  
+    $(document).mousemove(function (e) {
+      if (row_dragging) {
+        var percentage = (e.pageY / window.innerHeight) * 100;
+        var contBtmPercentage = 100 - percentage;
+  
+        $("#ckp-list").css("height", percentage + "%");
+        $("#embedding-plot").css("height", contBtmPercentage + "%");
+      }
+    });
+  });
+
+// mouseup handler for dragging
 $(document).mouseup(function (e) {
-  dragging = false;
-  $("#pdf-container").css("width", "100%");
+  if (col_dragging) {
+    col_dragging = false;
+    $("#pdf-container").css("width", "100%");
+  }
+  else if (row_dragging) {
+      row_dragging = false;
+  }
 });
