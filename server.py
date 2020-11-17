793eb1dbd573ce488b498fc8e96309bc5280b05a
JiangHe <jiangh0124@gmail.com>
2020-11-16 21:51:53 -0500

    able to call get_key_sentence api and show in new template

--- server.py
+++ server.py
@@ -11,6 +11,47 @@ er = EmbedRank(model_path=MODEL_PATH)
 def home():
     return render_template("index.html")
 
+@app.route("/get_key_phrases", methods=["GET", "POST"])
+def get_key_phrases():
+    '''
+    f = request.files["doc"]
+    f.save("test.pdf")
+    text = er.extract_information("test.pdf")
+    return text
+    '''
+    #pdf = open(request.files["doc"], "rb")
+    #text = er.extract_information(pdf)
+    res_dict = embed_rank_pipline("test_doc4.pdf")
+
+    return render_template("content.html", pdf_filename="test_doc4.pdf", data=res_dict)
+
+
+def reconstructor(sent_token, selected_ckp_strings, selected_sent_index):
+    '''
+    og_sents = [ (sent index, sent), ...]
+    selected_ckp_strings = [string1, string2, ...]
+    selected_full_sents = [(rank, og_sent, sent_index)]
+    zip_display = [(ckp1, (rank1, og_sent_for_ckp1, sent_index)), ...]
+    '''
+    res_dict = {
+        "og_sents" : [],
+        "selected_ckp_strings" : selected_ckp_strings,
+        "selected_full_sents" : [],
+        "zip_display" : []
+    }
+    
+    for sent_index in range(len(sent_token)):
+        res_dict["og_sents"].append((sent_index, sent_token[sent_index]))
+
+    rank = 1
+    for sent_index in selected_sent_index:
+        res_dict["selected_full_sents"].append((rank, sent_token[sent_index], sent_index))
+        rank += 1
+
+    res_dict["zip_display"] = list(zip(res_dict["selected_ckp_strings"], res_dict["selected_full_sents"]))
+
+    return res_dict
+
 def embed_rank_pipline(pdf_filename):
     text = er.extract_information(pdf_filename)
 
@@ -26,19 +67,7 @@ def embed_rank_pipline(pdf_filename):
     doc_embed, ckps_embed = er.embed_doc_ckps(pdf_filename, word_token)
     selected_ckp_strings, selected_sent_index = er.mmr(doc_embed, ckps_embed)
 
-@app.route("/get_key_phrases", methods=["GET", "POST"])
-def get_key_phrases():
-    '''
-    f = request.files["doc"]
-    f.save("test.pdf")
-    text = er.extract_information("test.pdf")
-    return text
-    '''
-    #pdf = open(request.files["doc"], "rb")
-    #text = er.extract_information(pdf)
-    names = ["hi", "no"]
-    return render_template("content.html", pdf_filename="test_doc4.pdf", names=names)
-
+    return reconstructor(sent_token, selected_ckp_strings, selected_sent_index)
 
 
 if __name__ == "__main__":
