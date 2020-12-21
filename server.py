37d87e930ea174f5a6c6f5f862b5c73c4e353fa1
JiangHe <jiangh0124@gmail.com>
2020-12-20 19:28:33 -0500

    fixed functions to use ckp filtered

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from six import get_function_globals
 from embed_rank.EmbedRank import EmbedRank
 from flask import Flask, request, render_template
 
@@ -37,17 +38,17 @@ def upload_file():
     f.save(FILE_NAME)
     return "Success"
 
-def reconstructor(sent_token, selected_ckp_strings, selected_sent_index):
+def reconstructor(sent_token, selected_ckp_filtered, selected_sent_index):
     '''
     og_sents = [ (sent index, sent), ...]
-    selected_ckp_strings = [string1, string2, ...]
+    selected_ckp_filtered, = [string1, string2, ...]
     selected_full_sents = [(rank, og_sent, sent_index)]
     zip_display = [(ckp1, (rank1, og_sent_for_ckp1, sent_index)), ...]
     '''
 
     res_dict = {
         "og_sents" : [],
-        "selected_ckp_strings" : selected_ckp_strings,
+        "selected_ckp_filtered" : selected_ckp_filtered,
         "selected_full_sents" : [],
         "zip_display" : []
     }
@@ -56,15 +57,12 @@ def reconstructor(sent_token, selected_ckp_strings, selected_sent_index):
         res_dict["og_sents"].append((sent_index, sent_token[sent_index]))
 
     rank = 1
-    seen_sent_index = set()
     for sent_index in selected_sent_index:
-        if sent_index not in seen_sent_index:
-            seen_sent_index.add(sent_index)
-            res_dict["selected_full_sents"].append((rank, sent_token[sent_index], sent_index))
-            rank += 1
+        res_dict["selected_full_sents"].append((rank, sent_token[sent_index], sent_index))
+        rank += 1
 
-    res_dict["zip_display"] = list(zip(res_dict["selected_ckp_strings"], res_dict["selected_full_sents"]))
-
+    res_dict["zip_display"] = list(zip(res_dict["selected_ckp_filtered"], res_dict["selected_full_sents"]))
+    
     return res_dict
 
 def embed_rank_pipline(pdf_filename):
@@ -80,9 +78,9 @@ def embed_rank_pipline(pdf_filename):
     assert len(sent_token) == len(word_token)
 
     doc_embed, ckps_embed = er.embed_doc_ckps(pdf_filename, word_token, mode="infer_mode")
-    selected_ckp_strings, selected_sent_index = er.mmr(doc_embed, ckps_embed)
+    selected_ckp_strings, selected_ckp_filtered, selected_sent_index = er.mmr(doc_embed, ckps_embed)
 
-    return reconstructor(sent_token, selected_ckp_strings, selected_sent_index)
+    return reconstructor(sent_token, selected_ckp_filtered, selected_sent_index)
 
 
 
